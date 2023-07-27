4353673e0c56456e74b343587b355d7555fae1a7
TBJsRealm <109115662+TBJsRealm@users.noreply.github.com>
2023-07-27 12:07:57 +1000

    Revert "Revert "20/07""
    This reverts commit e3985057e583a4f0cbd11b3d610702e62cac4dab.

--- Maintenance Manager/EditReport.cs
+++ Maintenance Manager/EditReport.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Maintenance_Manager
 {
@@ -17,13 +18,15 @@ namespace Maintenance_Manager
             InitializeComponent();
         }
 
+        public static string status;
+        public static int index;
         private void EditReport_Load(object sender, EventArgs e)
         {
             //get data to put into form
-            int index = ViewDataForm.index;
+            index = ViewDataForm.index;
             string title = LoginForm.reports[index].Title.ToString();
             string description = LoginForm.reports[index].Description.ToString();
-            string status = LoginForm.reports[index].Status.ToString();
+            status = LoginForm.reports[index].Status.ToString();
 
             lblTitle.Text = title;
             lblDescription.Text = description;
@@ -35,6 +38,10 @@ namespace Maintenance_Manager
             AHomePageForm Home = new AHomePageForm();
             Home.Show();
             this.Close();
+            SaveStatus();
+
+            //to show that its saved
+            MessageBox.Show("saved");
         }
 
         private void reportsToolStripMenuItem_Click(object sender, EventArgs e)
@@ -42,6 +49,50 @@ namespace Maintenance_Manager
             ViewDataForm ViewForm = new ViewDataForm();
             ViewForm.Show();
             this.Close();
+            SaveStatus();
+            //to show that its saved
+            MessageBox.Show("saved");
+        }
+
+        public void SaveStatus()
+        {
+            //get the changed status
+            status = cmbBxStatus.SelectedItem.ToString();
+
+            //file name ("AllReports.csv")
+            var file = "AllReports.csv";
+
+            using (var stream = File.CreateText(file))
+            {
+                for (int i = 0; i < LoginForm.reports.Count(); i++)
+                {
+                    if (i == index)
+                    {
+                        string title = LoginForm.reports[i].Title.ToString();
+                        string name = LoginForm.reports[i].Name.ToString();
+                        string id = LoginForm.reports[i].ID.ToString();
+                        string description = LoginForm.reports[i].Description.ToString();
+                        string urgency = LoginForm.reports[i].Priority.ToString();
+                        string date = LoginForm.reports[i].Date.ToString();
+                        string stat = status;
+                        string csvRow = string.Format("{0},{1},{2},{3},{4},{5},{6}", title, name, id, description, urgency, date, status);
+                        stream.WriteLine(csvRow);
+                    }
+                    else
+                    {
+                        string title = LoginForm.reports[i].Title.ToString();
+                        string name = LoginForm.reports[i].Name.ToString();
+                        string id = LoginForm.reports[i].ID.ToString();
+                        string description = LoginForm.reports[i].Description.ToString();
+                        string urgency = LoginForm.reports[i].Priority.ToString();
+                        string date = LoginForm.reports[i].Date.ToString();
+                        string stat = LoginForm.reports[i].Status.ToString();
+                        string csvRow = string.Format("{0},{1},{2},{3},{4},{5},{6}", title, name, id, description, urgency, date, status);
+                        stream.WriteLine(csvRow);
+                    }
+
+                }
+            }
         }
     }
 }
